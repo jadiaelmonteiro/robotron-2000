5949db80e881a3150267cbbda9d936912da98eb1
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2022-09-10 21:40:13 -0300

    Implemento da funcionalidade de setar os atributos do robotron

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-const shilding =  document.querySelector("#shield");
-var valorShield = parseInt(shilding.value);
+const controle = document.querySelectorAll("[data-controle]");
 
-function setShield() {
-    valorShield += 1;
-    shilding.value = valorShield
-    return shilding.value;
-}
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+});
 
+somar.addEventListener("click", () => { manipulaDados("somar") })
+
+subtrair.addEventListener("click", () => { manipulaDados("subtrair") });
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+};
